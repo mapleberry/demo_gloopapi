3786c773f60bb35e7b7a11a8bbe3a3c516530005
Olgierd Falat <olgierd.falat@gmail.com>
2016-09-25 18:57:40 +0100

    Created test stubs.

---
+++ server/test/unit/routes/user.spec.js
@@ -0,0 +1,19 @@
+'use strict'
+
+describe('user routes', () => {
+    it('GET /', () => {
+
+    })
+
+    it('POST /', () => {
+
+    })
+
+    it('PUT /', () => {
+
+    })
+
+    it('DELETE /', () => {
+
+    })
+})
