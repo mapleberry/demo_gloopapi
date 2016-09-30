df141ac99802e23725fd4dcec84af1ca69da9620
Olgierd Falat <olgierd.falat@gmail.com>
2016-09-30 20:02:56 +0100

    Added acceptance test for simulate-error endpoint.

--- server/test/acceptance-test/test/internal.spec.js
+++ server/test/acceptance-test/test/internal.spec.js
@@ -1,28 +1,34 @@
 const init = require('./init')
 const Promise = require('bluebird')
 const rp = require('request-promise')
+const chai = require('chai')
+const expect = chai.expect
 
 let baseUrl
 
 describe('/internal', function () {
-  this.timeout(5000)
-  before(() => {
-    return init().tap(url => {
-      baseUrl = url
+    before(() => {
+        return init().tap(url => {
+            baseUrl = url
+        })
     })
-  })
 
-  it('/healthcheck returns a 200 OK', () => {
-    return rp.get(`${baseUrl}/internal/healthcheck`)
-  })
+    it('/healthcheck returns a 200 OK', () => {
+        return rp.get(`${baseUrl}/internal/healthcheck`)
+    })
 
-  it('/log-config returns a 200 OK', () => {
-    return rp.get(`${baseUrl}/internal/log-config`)
-  })
+    it('/log-config returns a 200 OK', () => {
+        return rp.get(`${baseUrl}/internal/log-config`)
+    })
 
-    // it('/simulate-error returns a 500 status', () => {
-    //     return rp.get(`${baseUrl}/internal/simulate-error`)
-    //     init.then(r => r.get('/internal/simulate-error')
-    //         .expect(500, done))
-    // })
+    it('/simulate-error returns a 500 status', () => {
+        const options = {
+            uri: `${baseUrl}/internal/simulate-error`,
+            simple: false,
+            resolveWithFullResponse: true
+        }
+        return rp.get(options).then(res => {
+            expect(res.statusCode).to.equal(500)
+        })
+    })
 })
